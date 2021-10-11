6116adcb667150c8c0b18695280a9aaa259327be
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2021-10-11 20:54:22 +0900

    20211011

--- app/src/main/java/local/hal/st31/android/calculator_3schoolsjoint/MainActivity.java
+++ app/src/main/java/local/hal/st31/android/calculator_3schoolsjoint/MainActivity.java
@@ -3,6 +3,11 @@ package local.hal.st31.android.calculator_3schoolsjoint;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btClickText = findViewById(R.id.one);
+//        btClickText.setOnClickListener(new ButtonClickListener());
 
 
     }
